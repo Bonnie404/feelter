8bfda6dfadb9f8c70f541abb40117eda23a3e9f1
Bonnie <martin.purgat@gmail.com>
2025-06-13 23:19:39 +0200

    Removing accidental folder upload

--- install/dynamic_slam_interfaces/share/dynamic_slam_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(dynamic_slam_interfaces_IDL_FILES "msg/ObjectOdometry.idl;msg/ObjectOdometryPath.idl;msg/MultiObjectOdometryPath.idl")
-set(dynamic_slam_interfaces_INTERFACE_FILES "msg/ObjectOdometry.msg;msg/ObjectOdometryPath.msg;msg/MultiObjectOdometryPath.msg")
